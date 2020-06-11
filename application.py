6c30fda2a03048ce2d009ca9a4d1a85e905be889
YPrahasith <lohit2552@gmail.com>
2020-06-11 12:57:38 +0530

    last

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import time
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, current_user, logout_user
 from flask_socketio import SocketIO, join_room, leave_room, send
+from flask_migrate import Migrate
 
 from wtform_fields import *
 from models import *
@@ -16,6 +17,7 @@ app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Initialize login manager
 login = LoginManager(app)
--- create.py
+++ create.py
@@ -4,7 +4,7 @@ from models import *
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgres://xletpeppbqjdes:623134863aad13710d791e4fdb90e0a3e220786d5c7a53dcc818fafb7d323d0c@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/dfphimldchqdbj'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db.init_app(app)
